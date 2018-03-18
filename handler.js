a8210359c4737c3d4a4b007acac84814939ccece
syon <syon.xiii@gmail.com>
2018-03-18 16:33:21 +0900

    Initial commit

---
+++ handler.js
@@ -0,0 +1,18 @@
+module.exports.helloWorld = async (event, context, callback) => {
+  console.log("awaiting 5 seconds...");
+  await new Promise(r => setTimeout(r, 5000));
+  console.log("await done.");
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event
+    })
+  };
+
+  callback(null, response);
+};
